785f1e0c4320001116a6e1c510e96d11c08fd073
SzerlagDominika <73647769+SzerlagDominika@users.noreply.github.com>
2021-03-19 12:58:35 +0100

    Update script.js

--- 3JS/script.js
+++ 3JS/script.js
@@ -1,4 +1,4 @@
-console.log(THREE)
+console.log(THREE);
 
 const renderer = new THREE.WebGLRenderer({
     antialias: true
@@ -12,22 +12,20 @@ const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.inner
 camera.position.set ( 0, 0, 100 );
 camera.lookAt( 0, 0, 0 );
 
-let controls = new THREE.OrbitControls(camera, renderer.domElement);
+    let controls = new THREE.OrbitControls(camera, renderer.domElement);
 
     controls.addEventListener("change", renderer);
 
-renderer.render( scene, camera );
+ const LineMaterial = new THREE.LineBasicMaterial( { color: 0x00ffff } );
+//  const points = [];
+//  points.push( new THREE.Vector3( -10, 0, 0 ) );
+//  points.push( new THREE.Vector3( 0, 10, 0 ) );
+//  points.push( new THREE.Vector3( 0, -10, 0 ) );
 
-const LineMaterial = new THREE.LineBasicMaterial( { color: 0x00ffff } );
-// const points = [];
-// points.push( new THREE.Vector3( -10, 0, 0 ) );
-// points.push( new THREE.Vector3( 0, 10, 0 ) );
-// points.push( new THREE.Vector3( 0, -10, 0 ) );
-
-// for (let i = 1; i<15; i++) {
+//  for (var i=1; i<15; i++){
 //     points.push( new THREE.Vector3( -5*i, -2*i, 0 ) );
 //     points.push( new THREE.Vector3( 3*i, 3*i, 0 ) );
-// }
+//  }
 
 // const lineGeometry = new THREE.BufferGeometry().setFromPoints( points );
 // const line = new THREE.Line( lineGeometry, LineMaterial );
@@ -44,8 +42,7 @@ const light2 = new THREE.PointLight({color: 0x0fff00})
 scene.add(light2 );
 light2.position.set(-50,50,20)
 
-//MGŁA
-const near = 80;
+const near = 50;
 const far = 150;
 const color = 'lightpink';
 scene.fog = new THREE.Fog(color, near, far);
@@ -55,25 +52,30 @@ const colorWhite = new THREE.Color('hsl(106, 100%, 90%)')
 
 const width = 20;
 const height = 20;
-const depth = 120;
-const cubeGeometry = new THREE.BoxGeometry( width, height, depth);
+const depth = 80;
+const cubeGeometry = new THREE.BoxGeometry( width, height, depth );
 const cubeMaterial = new THREE.MeshPhongMaterial({
     color: colorWhite,
     shininess: 80
 })
 
-const cube = new THREE.Mesh( cubeGeometry, cubeMaterial);
-scene.add (cube);
+const cube = new THREE.Mesh ( cubeGeometry, cubeMaterial );
+scene.add( cube );
 
-cube.rotation.z = -100;
-cube.rotation.x = 20;
 
-renderer.render( scene, camera );
+cube.rotation.z = 0;
+cube.rotation.x = 10;
+
+cube.position.x = 0;
+cube.position.y = 0;
+cube.position.z = 0;
+// renderer.render( scene, camera );
+
 
 const animate = () => {
     requestAnimationFrame(animate)
-    cube.rotation.x +=.02
+    cube.rotation.x +=.03
+    cube.rotation.y +=.03
     renderer.render( scene, camera );
 }
-
-animate();
+animate()
